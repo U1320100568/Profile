777d532a3facb1662e161a0e4960c28b4a979c69
Roy <roy200230034004@gmail.com>
2019-06-10 02:49:02 +0800

    add animate

--- Scripts/profile.js
+++ Scripts/profile.js
@@ -1,19 +1,25 @@
 
-function settle(data){
-	//展示promise 只會呼叫一次resolve，可靠性和可控性
-	return new Promise(function(resolve,reject){
-		//TODO settle account
-		// throw new 'error'; // 若失敗調用reject
-		resolve();
-		// do something else
-		resolve();//bug 重複呼叫
-	});
-}
-function deduct(){
-	console.log("already charge money...");
-}
-function deductFail(){
-	console.log("Deduct Failed..");
-}
-settle({}).then(deduct,deductFail);//callback 附加在 promise物件
+// function settle(data){
+// 	//展示promise 只會呼叫一次resolve，可靠性和可控性
+// 	return new Promise(function(resolve,reject){
+// 		//TODO settle account
+// 		// throw new 'error'; // 若失敗調用reject
+// 		resolve();
+// 		// do something else
+// 		resolve();//bug 重複呼叫
+// 	});
+// }
+// function deduct(){
+// 	console.log("already charge money...");
+// }
+// function deductFail(){
+// 	console.log("Deduct Failed..");
+// }
+// settle({}).then(deduct,deductFail);//callback 附加在 promise物件
 
+
+var wow = new WOW({
+	offset:10
+});
+
+wow.init();
